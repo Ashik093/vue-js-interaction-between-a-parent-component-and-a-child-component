84399fd63b0c0b660cb4648542a1b8fa311620c2
Ashikur38 <mdashikurrahman18@gmail.com>
2023-08-20 21:34:24 +0600

    initial commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,47 @@
+<script setup>
+import MessageComponent from './MessageComponent.vue';
+defineProps({
+  messageFromParent: String
+});
+const emit= defineEmits(["messageEmitted"])
+const emitCustomMessage = () => {
+    emit('messageEmitted', "This message is from child. The button in the child component is clicked.");
+};
+</script>
+
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+      <MessageComponent :message="messageFromParent" title="Message From Parent" classProps="text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"/>
+      
+      <div
+        class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"
+      >
+        <a
+          href="#"
+          @click="emitCustomMessage"
+          class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        >
+          Click
+          <svg
+            class="w-3.5 h-3.5 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,26 @@
+<script  setup>
+    import { ref } from 'vue';
+    import ChildComponent from './ChildComponent.vue';
+    import MessageComponent from './MessageComponent.vue';
+    let messageFromParent = ref('Message from parent component to child component.')
+    let messageFromChild = ref('')
+    const  handleEmittedMessage=(emitedCustomMessage)=> {
+        messageFromChild.value = emitedCustomMessage;
+    }
+</script>
+
+<template>
+  
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        
+        <MessageComponent v-if="messageFromChild.length>0" :message="messageFromChild" title="Message From Child" classProps="text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800"/>
+        <ChildComponent :messageFromParent="messageFromParent" @messageEmitted="handleEmittedMessage"/>
+    </div>
+</section>
+
+</template>
+
+<style scoped>
+
+</style>
